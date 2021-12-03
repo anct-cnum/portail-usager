f44003a222ddf71baeab69107ac85dcee3b3fcca
Romain Cambonie <romain.cambonie@gmail.com>
2021-12-03 11:20:08 +0100

    refactored following suggestions

--- src/features/cartography/infrastructure/configuration/tokens/marker/markers.token.ts
+++ src/features/cartography/infrastructure/configuration/tokens/marker/markers.token.ts
@@ -1,39 +1,11 @@
 import { InjectionToken } from '@angular/core';
-import type { MarkerConfiguration } from './marker.configuration';
-import type { Icon } from 'leaflet';
-import { DivIcon, icon, Point as LeafletPoint } from 'leaflet';
-import type { MarkerProperties } from '../../../presentation/models';
-import type { Feature, Point } from 'geojson';
+import type { DivIconMarkerFactory, IconMarkerFactory } from './markers.factories';
+import { cnfsClusterMarkerFactory, cnfsMarkerFactory, usagerMarkerFactory } from './markers.factories';
 
-export const MARKERS_TOKEN: InjectionToken<Record<Marker, MarkerConfiguration>> = new InjectionToken<
-  Record<Marker, (feature?: Feature<Point, MarkerProperties>) => DivIcon | Icon>
->('markers.configuration');
+export type IconFactory = DivIconMarkerFactory | IconMarkerFactory;
 
-const HALF: number = 0.5;
-const ROUND_FALSE: boolean = false;
-
-const CNFS_MARKER_WIDTH_IN_PIXEL: number = 28.5;
-const CNFS_MARKER_HEIGTH_IN_PIXEL: number = 48;
-const CNFS_MARKER_DIMENSIONS: LeafletPoint = new LeafletPoint(
-  CNFS_MARKER_WIDTH_IN_PIXEL,
-  CNFS_MARKER_HEIGTH_IN_PIXEL,
-  ROUND_FALSE
-);
-
-const CNFS_MARKER_CLUSTER_WIDTH_IN_PIXEL: number = 31;
-const CNFS_MARKER_CLUSTER_HEIGTH_IN_PIXEL: number = 36;
-const CNFS_MARKER_CLUSTER_DIMENSIONS: LeafletPoint = new LeafletPoint(
-  CNFS_MARKER_CLUSTER_WIDTH_IN_PIXEL,
-  CNFS_MARKER_CLUSTER_HEIGTH_IN_PIXEL,
-  ROUND_FALSE
-);
-
-const USAGER_MARKER_WIDTH_IN_PIXEL: number = 16;
-const USAGER_MARKER_HEIGTH_IN_PIXEL: number = 16;
-const USAGER_MARKER_DIMENSIONS: LeafletPoint = new LeafletPoint(
-  USAGER_MARKER_WIDTH_IN_PIXEL,
-  USAGER_MARKER_HEIGTH_IN_PIXEL,
-  ROUND_FALSE
+export const MARKERS_TOKEN: InjectionToken<Record<Marker, IconFactory>> = new InjectionToken<Record<Marker, IconFactory>>(
+  'markers.configuration'
 );
 
 export enum Marker {
@@ -42,37 +14,8 @@ export enum Marker {
   Usager = 'usager'
 }
 
-export const MARKERS: Record<Marker, (feature?: Feature<Point, MarkerProperties>) => DivIcon | Icon> = {
-  [Marker.Cnfs]: (): Icon =>
-    icon({
-      iconAnchor: new LeafletPoint(CNFS_MARKER_DIMENSIONS.x * HALF, CNFS_MARKER_DIMENSIONS.y),
-      iconSize: CNFS_MARKER_DIMENSIONS,
-      iconUrl: 'assets/map/pin-cnfs.svg'
-    }),
-  // eslint-disable-next-line max-lines-per-function
-  [Marker.CnfsCluster]: (feature?: Feature<Point, MarkerProperties>): DivIcon =>
-    new DivIcon({
-      className: '',
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      html: `<div
-        style="width: 31px;
-               height: 36px;
-               font-family: Marianne, sans-serif;
-               font-weight: 600;
-               color: #ffff;
-               text-align: center;
-               background-image: url('./assets/map/pin-cnfs-cluster.svg');
-               background-repeat: no-repeat;
-               background-position: center;
-               background-size: contain;">
-      ${feature?.properties['point_count_abbreviated'] as number}</div>`,
-      iconAnchor: new LeafletPoint(CNFS_MARKER_CLUSTER_DIMENSIONS.x * HALF, CNFS_MARKER_CLUSTER_DIMENSIONS.y),
-      iconSize: CNFS_MARKER_CLUSTER_DIMENSIONS
-    }),
-  [Marker.Usager]: (): Icon =>
-    icon({
-      iconAnchor: new LeafletPoint(USAGER_MARKER_DIMENSIONS.x * HALF, USAGER_MARKER_DIMENSIONS.y),
-      iconSize: USAGER_MARKER_DIMENSIONS,
-      iconUrl: 'assets/map/pin-usager.svg'
-    })
+export const MARKERS: Record<Marker, IconFactory> = {
+  [Marker.Cnfs]: cnfsMarkerFactory,
+  [Marker.CnfsCluster]: cnfsClusterMarkerFactory,
+  [Marker.Usager]: usagerMarkerFactory
 };
--- src/features/cartography/infrastructure/presentation/components/leaflet-map/leaflet-map.component.ts
+++ src/features/cartography/infrastructure/presentation/components/leaflet-map/leaflet-map.component.ts
@@ -1,10 +1,10 @@
-import type { MapOptions as LeafletMapOptions, Layer, LatLng, Icon, DivIcon } from 'leaflet';
+import type { MapOptions as LeafletMapOptions, Layer, LatLng } from 'leaflet';
 import { geoJSON, latLng, map, Map as LeafletMap, marker, tileLayer } from 'leaflet';
 import type { AfterViewInit, ElementRef, OnChanges } from '@angular/core';
 import { ChangeDetectionStrategy, Component, Inject, Input, ViewChild } from '@angular/core';
 import type { MapOptionsPresentation, MarkerProperties } from '../../models';
 import { MARKERS_TOKEN } from '../../../configuration';
-import type { Marker } from '../../../configuration';
+import type { IconFactory, Marker } from '../../../configuration';
 import type { Feature, FeatureCollection, Point } from 'geojson';
 import { GeocodeAddressUseCase } from '../../../../use-cases/geocode-address/geocode-address.use-case';
 import { EMPTY_FEATURE_COLLECTION } from '../../models';
@@ -49,19 +49,18 @@ export class LeafletMapComponent implements AfterViewInit, OnChanges {
 
   public constructor(
     @Inject(MARKERS_TOKEN)
-    private readonly markersConfigurations: Record<Marker, (feature?: Feature<Point, MarkerProperties>) => DivIcon | Icon>
+    private readonly markersConfigurations: Record<Marker, IconFactory>
   ) {}
 
   private initMap(): void {
     this._map = map(this.mapContainer.nativeElement, this._mapOptions);
   }
 
-  public ngAfterViewInit(): void {
-    this.initMap();
+  private mapIsInitialized(): boolean {
+    return this._map instanceof LeafletMap;
   }
 
-  public ngOnChanges(): void {
-    if (!(this._map instanceof LeafletMap)) return;
+  private refreshMarkersLayer(): void {
     if (this._map.hasLayer(this._markersLayer)) this._map.removeLayer(this._markersLayer);
 
     this._markersLayer = geoJSON(this.markers, {
@@ -71,5 +70,13 @@ export class LeafletMapComponent implements AfterViewInit, OnChanges {
         marker(position, { icon: this.markersConfigurations[feature.properties.markerIconConfiguration as Marker](feature) })
     });
     this._map.addLayer(this._markersLayer);
+  }
+
+  public ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  public ngOnChanges(): void {
+    if (this.mapIsInitialized()) this.refreshMarkersLayer();
   }
 }
