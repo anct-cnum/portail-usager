4ffd199a60d2374574641f58f33404991e261a94
Marc Gavanier <marc.gavanier@gmail.com>
2021-12-14 16:04:08 +0100

    move view culling logic in cluster service

--- src/features/cartography/infrastructure/presentation/pages/cartography/cartography.presenter.ts
+++ src/features/cartography/infrastructure/presentation/pages/cartography/cartography.presenter.ts
@@ -4,18 +4,16 @@ import {
   cnfsCoreToPresentation,
   MarkersPresentation,
   MarkerProperties,
-  EMPTY_FEATURE_COLLECTION
 } from '../../models';
 import { Coordinates } from '../../../../core';
 import { map, Observable, switchMap } from 'rxjs';
 import { ListCnfsByRegionUseCase, ListCnfsPositionUseCase } from '../../../../use-cases';
 import { GeocodeAddressUseCase } from '../../../../use-cases/geocode-address/geocode-address.use-case';
-import { Feature, FeatureCollection, Point } from 'geojson';
+import { FeatureCollection, Point } from 'geojson';
 import { ClusterService } from '../../services/cluster.service';
 import { AnyGeoJsonProperty } from '../../../../../../environments/environment.model';
 import { combineLatestWith } from 'rxjs/operators';
 import { ViewBox } from '../../directives/leaflet-map-state-change';
-import { setMarkerIcon } from '../../pipes/marker-icon-helper';
 
 @Injectable()
 export class CartographyPresenter {
@@ -30,18 +28,10 @@ export class CartographyPresenter {
     FeatureCollection<Point, AnyGeoJsonProperty>,
     ViewBox
   ]) => MarkersPresentation {
-    return ([cnfsFeatureCollection, viewBox]: [FeatureCollection<Point, AnyGeoJsonProperty>, ViewBox]): MarkersPresentation => {
-      if (!this.clusterService.isReady) this.clusterService.load(cnfsFeatureCollection.features);
-
-      return {
-        features: this.viewCulling(viewBox).features.map(this.selectMarkerAtZoomLevel(viewBox)),
+    return ([cnfsFeatureCollection, viewBox]: [FeatureCollection<Point, AnyGeoJsonProperty>, ViewBox]): MarkersPresentation => ({
+        features: this.clusterService.onlyVisibleMarkers(cnfsFeatureCollection, viewBox),
         type: 'FeatureCollection'
-      };
-    };
-  }
-
-  private selectMarkerAtZoomLevel(viewBox: ViewBox): (feature: Feature<Point, AnyGeoJsonProperty>) => Feature<Point, MarkerProperties> {
-    return setMarkerIcon(this.clusterService.getMarkerAtZoomLevel(viewBox.zoomLevel));
+      });
   }
 
   public geocodeAddress$(addressToGeocode$: Observable<string>): Observable<Coordinates> {
@@ -58,38 +48,5 @@ export class CartographyPresenter {
     return this.listCnfsPositionUseCase
       .execute$()
       .pipe(map(cnfsCoreToPresentation), combineLatestWith(viewBox$), map(this.onlyVisibleMarkers()));
-  }
-
-  public viewCulling(viewBox?: ViewBox | null): FeatureCollection<Point, AnyGeoJsonProperty> {
-    if (viewBox == null || !this.clusterService.isReady) return EMPTY_FEATURE_COLLECTION;
-
-    return {
-      features: this.viewCullingGetFinalFeatures(viewBox),
-      type: 'FeatureCollection'
-    };
-  }
-
-  public viewCullingGetClusterLeaves(cluster: Feature<Point, AnyGeoJsonProperty>): Feature<Point, AnyGeoJsonProperty>[] {
-    if (cluster.properties['cluster'] === true) return this.clusterService.index.getLeaves(Number(cluster.id), Infinity);
-    return [cluster];
-  }
-
-  public viewCullingGetFinalFeatures(viewbox: ViewBox): Feature<Point, AnyGeoJsonProperty>[] {
-    return this.viewCullingGetFinalMarkersPositions(viewbox);
-  }
-
-  public viewCullingGetFinalMarkersPositions(viewbox: ViewBox): Feature<Point, AnyGeoJsonProperty>[] {
-    const clustersForViewbox: Feature<Point, AnyGeoJsonProperty>[] = this.clusterService.index.getClusters(
-      viewbox.boundingBox,
-      viewbox.zoomLevel
-    );
-
-    if (this.clusterService.exceedClusterZoomLevel(viewbox.zoomLevel)) {
-      return clustersForViewbox.flatMap((cluster: Feature<Point, AnyGeoJsonProperty>): Feature<Point, AnyGeoJsonProperty>[] =>
-        this.viewCullingGetClusterLeaves(cluster)
-      );
-    }
-
-    return clustersForViewbox;
   }
 }
