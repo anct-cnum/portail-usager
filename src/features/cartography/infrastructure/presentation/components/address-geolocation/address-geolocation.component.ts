839360425150f692cb5a9232e2ef141b96a88fe3
Marc Gavanier <marc.gavanier@gmail.com>
2022-01-25 18:53:34 +0100

    add search address suggestion dropdown

--- src/features/cartography/infrastructure/presentation/components/address-geolocation/address-geolocation.component.ts
+++ src/features/cartography/infrastructure/presentation/components/address-geolocation/address-geolocation.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { Coordinates } from '../../../../core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { AddressFoundPresentation } from '../../models';
 
 // TODO geocode et locate sont deux sujets différents qu'il serait bien de séparer dans des composants dédiés
 
@@ -10,11 +11,15 @@ import { FormControl, FormGroup } from '@angular/forms';
   templateUrl: './address-geolocation.component.html'
 })
 export class AddressGeolocationComponent {
+  public currentAddressIndex: number = 0;
+
   public readonly searchForm: FormGroup = new FormGroup({
     address: new FormControl('')
   });
 
+  @Input() public addressSuggestions: AddressFoundPresentation[] = [];
   @Output() public readonly addressToGeocode: EventEmitter<string> = new EventEmitter<string>();
+  @Output() public readonly searchAddress: EventEmitter<string> = new EventEmitter<string>();
   @Output() public readonly usagerAutolocate: EventEmitter<Coordinates> = new EventEmitter<Coordinates>();
 
   public emitLocation(position: GeolocationPosition): void {
@@ -37,5 +42,18 @@ export class AddressGeolocationComponent {
         alert(`Nous n'avons pas réussi à vous localiser : ${error.message}`);
       }
     );
+  }
+
+  public search(addressInput: string): void {
+    this.searchAddress.next(addressInput);
+  }
+
+  public setAddressSuggestion(label: string): void {
+    this.searchForm.get('address')?.setValue(label);
+    this.geocode();
+  }
+
+  public trackByAddressName(_: number, address: AddressFoundPresentation): string {
+    return `${address.label}-${address.context}`;
   }
 }
