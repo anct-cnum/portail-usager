80683a657dc08ec688fd761ce3f3449d7255451d
Marc Gavanier <marc.gavanier@gmail.com>
2022-04-13 16:01:37 +0200

    fix structure label alignment (#99)
    * fix structure label alignment
    
    * add branch environment and set beta api to staging

--- src/environments/environment.branch.ts
+++ src/environments/environment.branch.ts
@@ -4,7 +4,7 @@ import { Api, EnvironmentModel } from './models/environment.model';
 export const ENVIRONMENT: EnvironmentModel = {
   apisConfiguration: {
     [Api.AdresseDataGouv]: { domain: 'https://api-adresse.data.gouv.fr' },
-    [Api.ConseillerNumerique]: { domain: 'https://beta.api.conseiller-numerique.gouv.fr' }
+    [Api.ConseillerNumerique]: { domain: 'https://api.conseiller-numerique.gouv.fr' }
   },
   type: EnvironmentType.Production
 };
